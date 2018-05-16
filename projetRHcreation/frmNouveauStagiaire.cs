d7663b75169d4cf0c75c643c74a6fb4d606559a1
Barbra H <barbrahoudeville@gmail.com>
2018-05-16 17:26:40 +0200

    formulaire interimaire

---
+++ projetRHcreation/frmNouveauStagiaire.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projetRHcreation
+{
+    public partial class frmNouveauStagiaire : projetRHcreation.frmNouveauCollaborateur
+    {
+        public frmNouveauStagiaire()
+        {
+            InitializeComponent();
+        }
+    }
+}
