c126a2dba850d72798ba3d2c126e753bb41fcf66
Johanna JOo° <johanna-24@hotmail.fr>
2018-05-16 14:50:36 +0200

    créa form liste collaborateurs

---
+++ projetRHcreation/frmListeCollabo.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace projetRHcreation
+{
+    partial class frmListeCollabo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdListe = new System.Windows.Forms.DataGridView();
+            this.lblRecherche = new System.Windows.Forms.Label();
+            this.txtbRecherche = new System.Windows.Forms.TextBox();
+            this.btnRechercher = new System.Windows.Forms.Button();
+            this.btnTous = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.Nom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prénom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Contrat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Qualification = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dates = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grdListe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdListe
+            // 
+            this.grdListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdListe.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nom,
+            this.Prénom,
+            this.Contrat,
+            this.Qualification,
+            this.Dates});
+            this.grdListe.Location = new System.Drawing.Point(36, 118);
+            this.grdListe.Name = "grdListe";
+            this.grdListe.RowTemplate.Height = 24;
+            this.grdListe.Size = new System.Drawing.Size(861, 248);
+            this.grdListe.TabIndex = 0;
+            this.grdListe.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdListe_CellContentClick);
+            // 
+            // lblRecherche
+            // 
+            this.lblRecherche.AutoSize = true;
+            this.lblRecherche.Location = new System.Drawing.Point(33, 47);
+            this.lblRecherche.Name = "lblRecherche";
+            this.lblRecherche.Size = new System.Drawing.Size(77, 17);
+            this.lblRecherche.TabIndex = 1;
+            this.lblRecherche.Text = "Recherche";
+            // 
+            // txtbRecherche
+            // 
+            this.txtbRecherche.Location = new System.Drawing.Point(145, 47);
+            this.txtbRecherche.Name = "txtbRecherche";
+            this.txtbRecherche.Size = new System.Drawing.Size(526, 22);
+            this.txtbRecherche.TabIndex = 2;
+            // 
+            // btnRechercher
+            // 
+            this.btnRechercher.Location = new System.Drawing.Point(714, 42);
+            this.btnRechercher.Name = "btnRechercher";
+            this.btnRechercher.Size = new System.Drawing.Size(102, 33);
+            this.btnRechercher.TabIndex = 3;
+            this.btnRechercher.Text = "&Rechercher";
+            this.btnRechercher.UseVisualStyleBackColor = true;
+            // 
+            // btnTous
+            // 
+            this.btnTous.Location = new System.Drawing.Point(822, 42);
+            this.btnTous.Name = "btnTous";
+            this.btnTous.Size = new System.Drawing.Size(75, 33);
+            this.btnTous.TabIndex = 4;
+            this.btnTous.Text = "&Tous";
+            this.btnTous.UseVisualStyleBackColor = true;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(518, 415);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(95, 33);
+            this.btnAjouter.TabIndex = 5;
+            this.btnAjouter.Text = "&Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(635, 415);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(104, 33);
+            this.btnModifier.TabIndex = 6;
+            this.btnModifier.Text = "&Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(765, 415);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(86, 33);
+            this.btnAnnuler.TabIndex = 7;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // Nom
+            // 
+            this.Nom.HeaderText = "Nom";
+            this.Nom.Name = "Nom";
+            // 
+            // Prénom
+            // 
+            this.Prénom.HeaderText = "Prénom";
+            this.Prénom.Name = "Prénom";
+            // 
+            // Contrat
+            // 
+            this.Contrat.HeaderText = "Contrat";
+            this.Contrat.Name = "Contrat";
+            // 
+            // Qualification
+            // 
+            this.Qualification.HeaderText = "Qualification";
+            this.Qualification.Name = "Qualification";
+            // 
+            // Dates
+            // 
+            this.Dates.HeaderText = "Dates";
+            this.Dates.Name = "Dates";
+            // 
+            // frmListeCollabo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 497);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.btnTous);
+            this.Controls.Add(this.btnRechercher);
+            this.Controls.Add(this.txtbRecherche);
+            this.Controls.Add(this.lblRecherche);
+            this.Controls.Add(this.grdListe);
+            this.Name = "frmListeCollabo";
+            this.Text = "Liste des collaborateurs";
+            ((System.ComponentModel.ISupportInitialize)(this.grdListe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdListe;
+        private System.Windows.Forms.Label lblRecherche;
+        private System.Windows.Forms.TextBox txtbRecherche;
+        private System.Windows.Forms.Button btnRechercher;
+        private System.Windows.Forms.Button btnTous;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prénom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Contrat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Qualification;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dates;
+    }
+}
