608d5cbfd2a46e65da629bd8b2470df13aa13c3b
Barbra H <barbrahoudeville@gmail.com>
2018-05-16 15:58:22 +0200

    Création du formulaire CDD

---
+++ projetRHcreation/frmNouveauCollabCDD.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace projetRHcreation
+{
+    partial class frmNouveauCollabCDD
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label17 = new System.Windows.Forms.Label();
+            this.lblSalaire = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblSalaire);
+            this.groupBox2.Controls.Add(this.comboBox6);
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.label17);
+            this.groupBox2.Size = new System.Drawing.Size(667, 215);
+            this.groupBox2.Controls.SetChildIndex(this.txtSalaire, 0);
+            this.groupBox2.Controls.SetChildIndex(this.dateTimePicker1, 0);
+            this.groupBox2.Controls.SetChildIndex(this.comboBox4, 0);
+            this.groupBox2.Controls.SetChildIndex(this.comboBox3, 0);
+            this.groupBox2.Controls.SetChildIndex(this.comboBox2, 0);
+            this.groupBox2.Controls.SetChildIndex(this.comboBox5, 0);
+            this.groupBox2.Controls.SetChildIndex(this.label17, 0);
+            this.groupBox2.Controls.SetChildIndex(this.dateTimePicker2, 0);
+            this.groupBox2.Controls.SetChildIndex(this.label15, 0);
+            this.groupBox2.Controls.SetChildIndex(this.comboBox6, 0);
+            this.groupBox2.Controls.SetChildIndex(this.lblSalaire, 0);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.Location = new System.Drawing.Point(156, 126);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(448, 48);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(200, 21);
+            this.comboBox6.TabIndex = 21;
+            this.comboBox6.Text = "Accroissement d\'Activité";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(362, 56);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(36, 13);
+            this.label15.TabIndex = 20;
+            this.label15.Text = "Motif :";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(448, 22);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 19;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(362, 25);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(79, 13);
+            this.label17.TabIndex = 18;
+            this.label17.Text = "Fin de Contrat :";
+            // 
+            // lblSalaire
+            // 
+            this.lblSalaire.AutoSize = true;
+            this.lblSalaire.Location = new System.Drawing.Point(30, 156);
+            this.lblSalaire.Name = "lblSalaire";
+            this.lblSalaire.Size = new System.Drawing.Size(113, 13);
+            this.lblSalaire.TabIndex = 26;
+            this.lblSalaire.Text = "Salaire Brut Mensuel : ";
+            // 
+            // frmNouveauCollabCDD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(823, 678);
+            this.Name = "frmNouveauCollabCDD";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        protected System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label15;
+        protected System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label17;
+        protected System.Windows.Forms.Label lblSalaire;
+    }
+}
---
+++ projetRHcreation/frmNouveauCollabCDD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projetRHcreation
+{
+    public partial class frmNouveauCollabCDD : projetRHcreation.frmNouveauCollaborateur
+    {
+        public frmNouveauCollabCDD()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
