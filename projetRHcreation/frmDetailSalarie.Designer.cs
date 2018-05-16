361e5ea4360166760240489c91958f7366998ad7
matt.otin <matt.otin@gmail.com>
2018-05-16 17:08:19 +0200

    modif detail collabo

--- projetRHcreation/frmDetailSalarie.Designer.cs
+++ projetRHcreation/frmDetailSalarie.Designer.cs
@@ -28,18 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEditer = new System.Windows.Forms.Button();
+            this.btnAjoutContrat = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(100, 336);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(639, 95);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnEditer
+            // 
+            this.btnEditer.Location = new System.Drawing.Point(19, 364);
+            this.btnEditer.Name = "btnEditer";
+            this.btnEditer.Size = new System.Drawing.Size(65, 26);
+            this.btnEditer.TabIndex = 4;
+            this.btnEditer.Text = "&Editer";
+            this.btnEditer.UseVisualStyleBackColor = true;
+            // 
+            // btnAjoutContrat
+            // 
+            this.btnAjoutContrat.Location = new System.Drawing.Point(456, 471);
+            this.btnAjoutContrat.Name = "btnAjoutContrat";
+            this.btnAjoutContrat.Size = new System.Drawing.Size(92, 24);
+            this.btnAjoutContrat.TabIndex = 6;
+            this.btnAjoutContrat.Text = "&Ajout contrat";
+            this.btnAjoutContrat.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(573, 471);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(53, 25);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(654, 471);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(85, 24);
+            this.btnAnnuler.TabIndex = 8;
+            this.btnAnnuler.Text = "&Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
             // 
             // frmDetailSalarie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(831, 538);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnAjoutContrat);
+            this.Controls.Add(this.btnEditer);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "frmDetailSalarie";
+            this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.btnEditer, 0);
+            this.Controls.SetChildIndex(this.btnAjoutContrat, 0);
+            this.Controls.SetChildIndex(this.btnOK, 0);
+            this.Controls.SetChildIndex(this.btnAnnuler, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEditer;
+        private System.Windows.Forms.Button btnAjoutContrat;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnAnnuler;
     }
 }
