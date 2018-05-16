3116d7458d8be86c1edcb78355f675aac4275c2e
Barbra H <barbrahoudeville@gmail.com>
2018-05-16 16:04:26 +0200

    Modifs et regénération fois 15

--- projetRHcreation/frmNouveauCollaborateur.Designer.cs
+++ projetRHcreation/frmNouveauCollaborateur.Designer.cs
@@ -60,6 +60,8 @@
             this.label14 = new System.Windows.Forms.Label();
             this.comboBox5 = new System.Windows.Forms.ComboBox();
             this.txtSalaire = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -365,11 +367,31 @@
             this.txtSalaire.TabIndex = 25;
             this.txtSalaire.Text = "3300";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(629, 618);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Annuler";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 618);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmNouveauCollaborateur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(723, 550);
+            this.ClientSize = new System.Drawing.Size(723, 653);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txtMatricule);
@@ -389,7 +411,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
@@ -419,5 +440,8 @@
         private System.Windows.Forms.Label label14;
         protected System.Windows.Forms.ComboBox comboBox5;
         protected System.Windows.Forms.TextBox txtSalaire;
+        protected System.Windows.Forms.Button button2;
+        protected System.Windows.Forms.Button button1;
+        protected System.Windows.Forms.PictureBox pictureBox1;
     }
 }
