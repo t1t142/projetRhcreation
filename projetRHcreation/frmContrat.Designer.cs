2c28d8fd95c17d2ebc82197feb48cb9313467fbc
Johanna JOo° <johanna-24@hotmail.fr>
2018-05-17 09:21:23 +0200

    Dernières modifs form contrat

--- projetRHcreation/frmContrat.Designer.cs
+++ projetRHcreation/frmContrat.Designer.cs
@@ -28,21 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.grpContrat = new System.Windows.Forms.GroupBox();
-            this.lblTypeContrat = new System.Windows.Forms.Label();
-            this.lblDateDebut = new System.Windows.Forms.Label();
             this.lblMotif = new System.Windows.Forms.Label();
             this.lblAgenceInterim = new System.Windows.Forms.Label();
             this.lblEcole = new System.Windows.Forms.Label();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.grpContrat.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtMatricule
@@ -81,71 +75,62 @@
             // 
             // comboBox2
             // 
-            this.comboBox2.Location = new System.Drawing.Point(208, 54);
+            this.comboBox2.Location = new System.Drawing.Point(208, 36);
             this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox2.Size = new System.Drawing.Size(160, 24);
             this.comboBox2.Visible = false;
             // 
             // comboBox3
             // 
-            this.comboBox3.Location = new System.Drawing.Point(208, 121);
+            this.comboBox3.Location = new System.Drawing.Point(208, 134);
             this.comboBox3.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox3.Size = new System.Drawing.Size(160, 24);
             this.comboBox3.Visible = false;
             // 
             // comboBox4
             // 
-            this.comboBox4.Location = new System.Drawing.Point(208, 88);
+            this.comboBox4.Location = new System.Drawing.Point(208, 101);
             this.comboBox4.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox4.Size = new System.Drawing.Size(160, 24);
             this.comboBox4.Visible = false;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(208, 22);
+            this.dateTimePicker1.Location = new System.Drawing.Point(208, 69);
             this.dateTimePicker1.Visible = false;
             // 
             // groupBox1
             // 
-            this.groupBox1.Size = new System.Drawing.Size(732, 151);
+            this.groupBox1.Size = new System.Drawing.Size(497, 151);
             this.groupBox1.Visible = false;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.grpContrat);
-            this.groupBox2.Location = new System.Drawing.Point(28, 33);
-            this.groupBox2.Size = new System.Drawing.Size(753, 202);
+            this.groupBox2.Location = new System.Drawing.Point(23, 29);
+            this.groupBox2.Size = new System.Drawing.Size(732, 244);
             this.groupBox2.Text = "Contrat";
-            this.groupBox2.Controls.SetChildIndex(this.label12, 0);
-            this.groupBox2.Controls.SetChildIndex(this.dateTimePicker1, 0);
-            this.groupBox2.Controls.SetChildIndex(this.comboBox4, 0);
-            this.groupBox2.Controls.SetChildIndex(this.comboBox3, 0);
-            this.groupBox2.Controls.SetChildIndex(this.comboBox2, 0);
-            this.groupBox2.Controls.SetChildIndex(this.comboBox5, 0);
-            this.groupBox2.Controls.SetChildIndex(this.txtSalaire, 0);
-            this.groupBox2.Controls.SetChildIndex(this.grpContrat, 0);
             // 
             // comboBox5
             // 
-            this.comboBox5.Location = new System.Drawing.Point(208, 155);
+            this.comboBox5.Location = new System.Drawing.Point(208, 168);
             this.comboBox5.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox5.Size = new System.Drawing.Size(160, 24);
             this.comboBox5.Visible = false;
             // 
             // txtSalaire
             // 
-            this.txtSalaire.Location = new System.Drawing.Point(208, 187);
+            this.txtSalaire.Location = new System.Drawing.Point(208, 200);
             this.txtSalaire.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtSalaire.Visible = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(681, 452);
+            this.button2.Location = new System.Drawing.Point(655, 450);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(573, 452);
+            this.button1.Location = new System.Drawing.Point(547, 450);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             // 
             // pictureBox1
@@ -155,43 +140,32 @@
             // 
             // label12
             // 
-            this.label12.Location = new System.Drawing.Point(40, 30);
+            this.label12.Location = new System.Drawing.Point(40, 74);
             // 
-            // grpContrat
+            // label1
             // 
-            this.grpContrat.Controls.Add(this.dateTimePicker2);
-            this.grpContrat.Controls.Add(this.comboBox6);
-            this.grpContrat.Controls.Add(this.lblDateDebut);
-            this.grpContrat.Controls.Add(this.lblTypeContrat);
-            this.grpContrat.Location = new System.Drawing.Point(1, 4);
-            this.grpContrat.Name = "grpContrat";
-            this.grpContrat.Size = new System.Drawing.Size(752, 205);
-            this.grpContrat.TabIndex = 26;
-            this.grpContrat.TabStop = false;
-            this.grpContrat.Text = "Contrat";
+            this.label1.Visible = false;
             // 
-            // lblTypeContrat
+            // label9
             // 
-            this.lblTypeContrat.AutoSize = true;
-            this.lblTypeContrat.Location = new System.Drawing.Point(20, 60);
-            this.lblTypeContrat.Name = "lblTypeContrat";
-            this.lblTypeContrat.Size = new System.Drawing.Size(108, 17);
-            this.lblTypeContrat.TabIndex = 0;
-            this.lblTypeContrat.Text = "Type de contrat";
+            this.label9.Location = new System.Drawing.Point(40, 39);
             // 
-            // lblDateDebut
+            // label10
             // 
-            this.lblDateDebut.AutoSize = true;
-            this.lblDateDebut.Location = new System.Drawing.Point(20, 113);
-            this.lblDateDebut.Name = "lblDateDebut";
-            this.lblDateDebut.Size = new System.Drawing.Size(98, 17);
-            this.lblDateDebut.TabIndex = 1;
-            this.lblDateDebut.Text = "Date de début";
+            this.label10.Location = new System.Drawing.Point(40, 137);
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(40, 104);
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(40, 171);
             // 
             // lblMotif
             // 
             this.lblMotif.AutoSize = true;
-            this.lblMotif.Location = new System.Drawing.Point(150, 278);
+            this.lblMotif.Location = new System.Drawing.Point(150, 293);
             this.lblMotif.Name = "lblMotif";
             this.lblMotif.Size = new System.Drawing.Size(38, 17);
             this.lblMotif.TabIndex = 17;
@@ -200,7 +174,7 @@
             // lblAgenceInterim
             // 
             this.lblAgenceInterim.AutoSize = true;
-            this.lblAgenceInterim.Location = new System.Drawing.Point(150, 330);
+            this.lblAgenceInterim.Location = new System.Drawing.Point(150, 345);
             this.lblAgenceInterim.Name = "lblAgenceInterim";
             this.lblAgenceInterim.Size = new System.Drawing.Size(113, 17);
             this.lblAgenceInterim.TabIndex = 18;
@@ -209,44 +183,29 @@
             // lblEcole
             // 
             this.lblEcole.AutoSize = true;
-            this.lblEcole.Location = new System.Drawing.Point(150, 382);
+            this.lblEcole.Location = new System.Drawing.Point(150, 397);
             this.lblEcole.Name = "lblEcole";
             this.lblEcole.Size = new System.Drawing.Size(43, 17);
             this.lblEcole.TabIndex = 19;
             this.lblEcole.Text = "Ecole";
             // 
-            // comboBox6
-            // 
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(165, 60);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(203, 24);
-            this.comboBox6.TabIndex = 2;
-            // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(165, 112);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker2.TabIndex = 3;
-            // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(313, 278);
+            this.textBox7.Location = new System.Drawing.Point(313, 293);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(233, 22);
             this.textBox7.TabIndex = 20;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(313, 333);
+            this.textBox8.Location = new System.Drawing.Point(313, 348);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(233, 22);
             this.textBox8.TabIndex = 21;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(313, 382);
+            this.textBox9.Location = new System.Drawing.Point(313, 397);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(233, 22);
             this.textBox9.TabIndex = 22;
@@ -254,7 +213,7 @@
             // frmContrat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.ClientSize = new System.Drawing.Size(810, 508);
+            this.ClientSize = new System.Drawing.Size(783, 508);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
@@ -264,6 +223,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmContrat";
             this.Text = "Ajout / modification de contrat de travail";
+            this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.txtMatricule, 0);
             this.Controls.SetChildIndex(this.groupBox1, 0);
             this.Controls.SetChildIndex(this.groupBox2, 0);
@@ -280,20 +240,12 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.grpContrat.ResumeLayout(false);
-            this.grpContrat.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox grpContrat;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.Label lblDateDebut;
-        private System.Windows.Forms.Label lblTypeContrat;
         private System.Windows.Forms.Label lblMotif;
         private System.Windows.Forms.Label lblAgenceInterim;
         private System.Windows.Forms.Label lblEcole;
